4811e990146fabbef56b109dda84dd768df83eae
ishanki19-pixel <81527206+ishanki19-pixel@users.noreply.github.com>
2023-04-30 11:54:29 +0530

    yet another commmit

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s5o=@nel#9uk&@*v@l(th2vrqi+i)icv+%2nz@x$aku62&r5+7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
